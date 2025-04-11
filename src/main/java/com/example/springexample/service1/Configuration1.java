8e3a32e8920d521f291c56987d9a3dbdafe07dc3
pnovich <pppp_84@ukr.net>
2025-04-11 19:55:30 +0300

    moved defining beans to configuration

--- src/main/java/com/example/springexample/service1/Configuration1.java
+++ src/main/java/com/example/springexample/service1/Configuration1.java
@@ -1,15 +1,16 @@
 package com.example.springexample.service1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configuration1 {
-//    @Bean
-//    InfoProviderImpl1 infoProviderImpl1() {
-//        return new InfoProviderImpl1();
-//    }
-//    @Bean
-//    InfoProviderImpl2 infoProviderImpl2() {
-//        return new InfoProviderImpl2();
-//    }
+    @Bean
+    InfoProviderImpl1 infoProviderImpl1() {
+        return new InfoProviderImpl1();
+    }
+    @Bean
+    InfoProviderImpl2 infoProviderImpl2() {
+        return new InfoProviderImpl2();
+    }
 }
--- src/main/java/com/example/springexample/service1/InfoProviderImpl2.java
+++ src/main/java/com/example/springexample/service1/InfoProviderImpl2.java
@@ -2,7 +2,7 @@ package com.example.springexample.service1;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class InfoProviderImpl2 implements InfoProvider {
     @Override
     public String getInfoFromProvider() {
